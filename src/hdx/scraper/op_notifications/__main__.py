2b6c5e8f4cfe0c21b0757f5f86cc4395c13efa6d
Briar <briar.j.mills@gmail.com>
2024-10-30 13:36:40 -0400

    Create process

--- src/hdx/scraper/op_notifications/__main__.py
+++ src/hdx/scraper/op_notifications/__main__.py
@@ -10,32 +10,60 @@ from os.path import dirname, expanduser, join
 
 from hdx.api.configuration import Configuration
 from hdx.facades.infer_arguments import facade
+from hdx.utilities.downloader import Download
+from hdx.utilities.errors_onexit import ErrorsOnExit
+from hdx.utilities.retriever import Retrieve
+from hdx.utilities.path import temp_dir
+from op_notifications import OPNotifications
 
 logger = logging.getLogger(__name__)
 
 _USER_AGENT_LOOKUP = "hdx-hapi-op-notifications"
+_SAVED_DATA_DIR = "saved_data"  # Keep in repo to avoid deletion in /tmp
 
 
-def main() -> None:
-    """Generate datasets and create them in HDX
+def main(
+    save: bool = True,
+    use_saved: bool = False,
+) -> None:
 
-    Args:
-        save (bool): Save downloaded data. Defaults to True.
-        use_saved (bool): Use saved data. Defaults to False.
+    with ErrorsOnExit() as errors_on_exit:
+        with temp_dir() as temp_folder:
+            with Download() as downloader:
+                with open("errors.txt", "w") as fp:
+                    pass
+                retriever = Retrieve(
+                    downloader=downloader,
+                    fallback_dir=temp_folder,
+                    saved_dir=_SAVED_DATA_DIR,
+                    temp_dir=temp_folder,
+                    save=save,
+                    use_saved=use_saved,
+                )
+                configuration = Configuration.read()
 
-    Returns:
-        None
-    """
-    configuration = Configuration.read()
+                op_notifications = OPNotifications(
+                    configuration,
+                    retriever,
+                    temp_folder,
+                    errors_on_exit,
+                )
+                op_notifications.read_yaml()
+                op_notifications.check_hdx()
+
+                if len(errors_on_exit.errors) > 0:
+                    errors = errors_on_exit.errors
+                    errors = ["The following datasets mey need updating"] + sorted(errors)
+                    with open("errors.txt", "w") as fp:
+                        fp.writelines(_ + " | " for _ in errors)
+                logger.info("Finished processing")
 
 
 if __name__ == "__main__":
     facade(
         main,
         hdx_site="dev",
-        hdx_read_only=True,
         user_agent_config_yaml=join(expanduser("~"), ".useragents.yaml"),
         user_agent_lookup=_USER_AGENT_LOOKUP,
-        project_config_yaml=join(
-            dirname(__file__), "config", "project_configuration.yaml"),
+        project_config_yaml=join(dirname(__file__), "config", "project_configuration.yaml"),
     )
--- src/hdx/scraper/op_notifications/op_notifications.py
+++ src/hdx/scraper/op_notifications/op_notifications.py
@@ -5,14 +5,39 @@ import logging
 
 from hdx.api.configuration import Configuration
 from hdx.data.dataset import Dataset
-from hdx.data.hdxobject import HDXError
+from hdx.utilities.errors_onexit import ErrorsOnExit
+from hdx.utilities.retriever import Retrieve
 
 logger = logging.getLogger(__name__)
 
 
 class OPNotifications:
     def __init__(
-        self, configuration: Configuration
+        self, configuration: Configuration, retriever: Retrieve, temp_dir: str, errors: ErrorsOnExit
     ):
         self._configuration = configuration
+        self._retriever = retriever
+        self._temp_dir = temp_dir
+        self.data = {}
+        self.errors = errors
 
+    def read_yaml(self):
+        base_url = self._configuration["config_url"]
+        op_yaml = self._retriever.download_yaml(base_url)
+        for admin_level in ["admintwo", "adminone", "national"]:
+            for pipeline_name, config in op_yaml[f"operational_presence_{admin_level}"].items():
+                self.data[pipeline_name] = {
+                    "dataset": config["dataset"],
+                    "resource": config["resource"],
+                }
+
+    def check_hdx(self):
+        for pipeline_name, dataset_info in self.data.items():
+            countryiso3 = pipeline_name.split("_")[-1]
+            dataset_name = dataset_info["dataset"]
+            dataset = Dataset.read_from_hdx(dataset_name)
+            resource_names = [r["name"] for r in dataset.get_resources()]
+            expected_index = self._configuration["resource_index_exceptions"].get(countryiso3, 0)
+            resource_index = resource_names.index(dataset_info["resource"])
+            if resource_index != expected_index:
+                self.errors.add(f"{countryiso3}: {dataset_name}")
